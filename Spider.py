9162396c4a3df6c28168acf3afa38efc6bff1d01
DJ <1062102863@qq.com>
2019-07-24 09:10:07 +0800

    Update Spider.py

--- Spider.py
+++ Spider.py
@@ -10,19 +10,22 @@ monkey.patch_all()
 import gevent
 from gevent.pool import Pool
 
-from ..DjSpider.DBcrud import Mysql_crud, Redis_crud, Mongo_crud, Switch_db
+from DjSpider.DBcrud import Mysql_crud, Redis_crud, Mongo_crud, Switch_db
 
 
 class DjSpider(object):
-    def __init__(self, db):
-        if db == 'none':
-            print('db is not use')
+    def __init__(self, db=None):
+        if db is None:
+            print("db is not use")
         elif db == "mysql":
             self.mysql = Mysql_crud()
         elif db == "redis":
             self.redis = Redis_crud()
         elif db == "mongo":
             self.mongo = Mongo_crud()
+        elif db == "mysql2redis" or "redis2mysql":
+            self.mysql = Mysql_crud()
+            self.redis = Redis_crud()
         elif db == "switch":
             self.switch = Switch_db()
         else:
@@ -32,7 +35,7 @@ class DjSpider(object):
             self.switch = Switch_db()
 
     def mysql_insert(self, table, data):
-        self.mysql.mysql_insert_data(table, data)
+        return self.mysql.mysql_insert_data(table, data)
 
     def mysql_output(self, sql):
         return self.mysql.mysql_output_data(sql)
@@ -76,33 +79,39 @@ class DjSpider(object):
 
     def process_pool(self, task, val=None, pool=4):
         p = ProcessPoolExecutor(pool)
-        for i in range(pool):
-            p.submit(task, val)
+        v = len(val) if isinstance(val, list) else pool
+        for i in range(v):
+            p.submit(task, i)
         p.shutdown()
 
     def thread_pool(self, task, val=None, pool=4):
         t = ThreadPoolExecutor(pool)
-        for i in range(pool):
-            t.submit(task, val)
+        v = len(val) if isinstance(val, list) else pool
+        for i in range(v):
+            t.submit(task, i)
         t.shutdown()
 
     def coroutine(self, task, val=None, pool=4):
         p = Pool(pool)
         jobs = []
-        for i in range(pool):
-            job = p.spawn(task, val)
+        v = len(val) if isinstance(val, list) else pool
+        for i in range(v):
+            job = p.spawn(task, i)
             jobs.append(job)
         gevent.joinall(jobs)
 
-    def get(self, url, headers=None, data=None, cookies=None, proxies=None, again=3):
+    def get(self, url, headers=None, data=None, cookies=None, proxies=None, status_code=True, again=3):
         times = 0
         while times < again:
             try:
                 resp = requests.get(url, headers=headers, data=data, cookies=cookies, proxies=proxies)
-                if resp.reason == 'OK':
-                    return resp
+                if status_code is True:
+                    if resp.status_code == 200:
+                        return resp
+                    else:
+                        raise TimeoutError('can not open')
                 else:
-                    raise TimeoutError('can not open')
+                    return resp
             except Exception as e:
                 print(e)
                 time.sleep(random.randint(1, 5))
@@ -111,15 +120,18 @@ class DjSpider(object):
                 if times == again:
                     break
 
-    def post(self, url, data=None, json=None, cookies=None, proxies=None, again=3):
+    def post(self, url, headers=None, data=None, json=None, cookies=None, proxies=None, status_code=True, again=3):
         times = 0
         while times < again:
             try:
-                resp = requests.post(url, data=data, json=json, cookies=cookies, proxies=proxies)
-                if resp.reason == 'OK':
-                    return resp
+                resp = requests.post(url, headers=headers, data=data, json=json, cookies=cookies, proxies=proxies)
+                if status_code is True:
+                    if resp.status_code == 200:
+                        return resp
+                    else:
+                        raise TimeoutError('can not open')
                 else:
-                    raise TimeoutError('can not open')
+                    return resp
             except Exception as e:
                 print(e)
                 time.sleep(random.randint(1, 5))
@@ -128,19 +140,18 @@ class DjSpider(object):
                 if times == again:
                     break
 
-    def Request(self, url, callback=None, method='GET', headers=None, data=None, json=None, cookies=None, proxies=None,
-                again=3):
+    def Request(self, url, callback=None, method='GET', headers=None, data=None, json=None, cookies=None, proxies=None, status_code=True, again=3):
         if callback is not None and not callable(callback):
             raise TypeError('callback must be a callable, got %s' % type(callback).__name__)
         method = str(method).upper()
         if method == 'GET':
-            resp = self.get(url, headers, data, cookies, proxies, again)
+            resp = self.get(url, headers, data, cookies, proxies, status_code, again)
             if callback:
                 return callback(resp)
             else:
                 return resp
         if method == 'POST':
-            resp = self.post(url, data, json, cookies, proxies, again)
+            resp = self.post(url, headers, data, json, cookies, proxies, status_code, again)
             if callback:
                 return callback(resp)
             else:
